ecdf2bc52957484a75a795654a1915566bff5cf6
Kyla-Yang <yiningy.21@intl.zju.edu.cn>
2022-03-22 16:25:57 +0800

    Create variables.py

---
+++ practical5/variables.py
@@ -0,0 +1,18 @@
+a=19245301
+b=4218520
+c=271
+d=b-c
+e=a-b
+if d>e:
+    print("2020 is greater")
+elif d<e:
+    print("2021 is greater")
+else:
+    print("2021 is equal to 2020")    
+    
+X="hello"
+Y="world"
+W=X and Y
+print(W)
+W=X or Y
+print(W)
