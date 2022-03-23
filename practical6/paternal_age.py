77bd26ceb960c364781a1f2157ac1e8eb15062d4
Kyla-Yang <yiningy.21@intl.zju.edu.cn>
2022-03-23 18:34:45 +0800

    Create paternal_age.py

---
+++ practical6/paternal_age.py
@@ -0,0 +1,5 @@
+import numpy as np
+import matplotlib.pyplot as plt
+paternal_age=[30,35,40,45,50,55,60,65,70,75]
+chd=[1.03,1.07,1.11,1.17,1.23,1.32,1.42,1.55,1.72,1.94]
+plt.scatter(paternal_age,chd,marker='o')
