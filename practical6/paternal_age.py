aaab6fc662daa34b31ccda6c045add2001c3a0b5
Kyla-Yang <yiningy.21@intl.zju.edu.cn>
2022-05-25 11:16:07 +0800

    Update paternal_age.py

--- practical6/paternal_age.py
+++ practical6/paternal_age.py
@@ -4,14 +4,14 @@ chd=[1.03,1.07,1.11,1.17,1.23,1.32,1.42,1.55,1.72,1.94]
 
 vs = dict(zip(paternal_age,chd))
 print(vs)
-#This method was taught by my friend Zhu Hengyu and he found this from https://blog.csdn.net/Kefenggewu_/article/details/123522854.
+#This method was told by my friend Zhu Hengyu and he found this from https://blog.csdn.net/Kefenggewu_/article/details/123522854.
 plt.scatter(paternal_age,chd,marker='o')
 plt.xlabel('paternal_age')
 plt.ylabel('chd')
 plt.title('paternal_age vs chd')
 #label the x and y axises
 plt.show()
-#determine the type of form (boxplot) and command it to draw
+#determine the form and command it to draw it
 age = 30
 #create a variable storing the input age
 print(vs[age])
